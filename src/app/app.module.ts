2719eec3dd7e3b6d2c77aa1853316e65b302ee7b
Nmuta Jones <nmuta1@gmail.com>
2021-01-16 01:21:11 +0300

    http setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FlightsComponent } from './flights/flights.component';
 import { AdminComponent } from './admin/admin.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AdminComponent } from './admin/admin.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/flight.service.ts
+++ src/app/flight.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Flight } from './flights/flight.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
